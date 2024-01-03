2a89e5b6977faab906c499cb3a8165bfe7c25546
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-03 02:43:58 +0100

    Update-on-30-days-of-javascript

---
+++ Day_5/script.js
@@ -0,0 +1,16 @@
+alert('Hello, World! I am a Programmer');
+const country = 'Nigeria';
+const continent = 'Africa';
+let population = '200 Million';
+
+console.log('I am from', country, 'in', continent, 'and we are', population, 'in population in my Country');
+
+let isIsland = 'True';
+let language = 'English';
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
+console.log(language);
