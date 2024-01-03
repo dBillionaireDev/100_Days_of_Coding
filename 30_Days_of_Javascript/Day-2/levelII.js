559e299e45ce1cdd774d206050db429a89b9d398
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-03 10:02:31 +0100

    new file

---
+++ 30_Days_of_Javascript/Day-2/levelII.js
@@ -0,0 +1,3 @@
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+console.log('"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."');
+
