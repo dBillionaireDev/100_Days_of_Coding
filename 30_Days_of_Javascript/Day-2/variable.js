77378d65a13d8af5922465563bb54b7837e45d45
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-03 09:31:52 +0100

    Modified variable.js file

--- 30_Days_of_Javascript/Day-2/variable.js
+++ 30_Days_of_Javascript/Day-2/variable.js
@@ -1,4 +1,25 @@
 let challenge = '30 Days Of Javascript.';
 console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substr(3,18));
+console.log(challenge.substring(0,2));
+console.log(challenge.includes('Script'));
+console.log(challenge.split('a'));
+console.log(challenge.split(' '));
+
+let bigTech = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(bigTech);
+console.log(bigTech.split(','));
+console.log(challenge.replace('30 Days of Javascript', '30 Days of Python'));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt(11));
+console.log(challenge.indexOf('a'));
+console.log(challenge.lastIndexOf('a'));
+
+let conjunction = 'You cannot end a sentence with because because because is a conjunction';
+console.log(conjunction.indexOf('because'));
+console.log(conjunction.lastIndexOf('because'));
 
 
