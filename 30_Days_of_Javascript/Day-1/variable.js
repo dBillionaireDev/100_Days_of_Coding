47531942a0ac46c3475bd0f68dbc4aa52e244aab
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-03 08:24:59 +0100

    Modified variable.js file

--- 30_Days_of_Javascript/Day-1/variable.js
+++ 30_Days_of_Javascript/Day-1/variable.js
@@ -3,3 +3,23 @@ let age = 23;
 let language;
 let job = false;
 
+let hope;
+let peace;
+let joy;
+let faith;
+
+let love = 'Zen';
+let height = "5'9";
+let weight = '65kg';
+let color = 'dark';
+
+\\Multiple lines personal variables
+let firstName = 'Ebenezer';
+let lastName = 'Ekunke';
+let maritalStatus = 'Single';
+let country = 'Nigeria';
+let age = 23;
+
+\\Single line personal variables
+let firstName = 'Ebenezer', lastName = 'Ekunke', maritalStatus = 'Single', country = 'Nigeria', age = 23;
+
