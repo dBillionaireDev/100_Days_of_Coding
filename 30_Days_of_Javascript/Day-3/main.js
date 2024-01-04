075842f8889c158fca6b2e686e0bae2eb4029f22
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-04 14:56:03 +0100

    Finished stage one of the challenge

--- 30_Days_of_Javascript/Day-3/main.js
+++ 30_Days_of_Javascript/Day-3/main.js
@@ -110,4 +110,25 @@ console.log(!(4 === '4'));
 console.log(goat.includes('on'));
 console.log(snake.includes('on'));
 
-alert('Task six is done');
+/* Task 7: Use the Date object to do the following activities
+
+What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now. */
+
+console.log('Task 7');
+
+const time = new Date();
+console.log(time.getFullYear());
+console.log(time.getMonth());
+console.log(time.getDate());
+console.log(time.getDay());
+console.log(time.getHours());
+console.log(time.getMinutes());
+console.log(time.getTime());
+
+alert('Task seven is done');
