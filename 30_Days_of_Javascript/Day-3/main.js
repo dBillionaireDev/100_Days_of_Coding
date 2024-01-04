455f95042600f4d686e00ccb4c74e7d06fe1e4b6
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-04 13:34:37 +0100

    main.js

--- 30_Days_of_Javascript/Day-3/main.js
+++ 30_Days_of_Javascript/Day-3/main.js
@@ -1,1 +1,20 @@
 // this is your main.js script
+
+//Task 1: Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+const firstName = 'Nezer'; 
+const lastName = 'Ekunke'; 
+const country = 'Nigeria'; 
+let city = 'Abuja'; 
+let age = 23; 
+let isMarried = false;
+let year = 2023;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+alert('Task one is done');
