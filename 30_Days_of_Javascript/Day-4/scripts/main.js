474e341a345087cf3e508afdc84b2ea959d31434
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-06 15:41:57 +0100

    Task two done

--- 30_Days_of_Javascript/Day-4/scripts/main.js
+++ 30_Days_of_Javascript/Day-4/scripts/main.js
@@ -81,3 +81,54 @@ case num <= 59 >= 50:
   default:
     console.log('You have no grade');
 }
+
+/*Task 2: Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer*/
+
+let userInput = prompt('What month is it?')
+let month = userInput.toLowerCase()
+
+switch (month) {
+ case 'september':
+    console.log('The season is Autumn')
+    break
+ case 'october': 
+    console.log('The season is Autumn')
+    break
+ case 'november':
+    console.log('The season is Autumn')
+    break
+ case 'december':
+    console.log('The season is Winter')
+    break
+ case 'january':
+    console.log('The season is Winter')
+    break
+ case 'february':
+    console.log('The season is Winter')
+    break
+ case 'march':
+    console.log('The season is Spring')
+    break
+ case 'april':
+    console.log('The season is Spring')
+    break
+  case 'may':
+    console.log('The season is Spring')
+    break
+  case 'june':
+    console.log('The season is Summer')
+    break
+  case 'july':
+    console.log('The season is Summer')
+    break
+  case 'august':
+    console.log('The season is Summer')
+    break
+  default:
+    console.log(`${month} is not a month.`)
+}
+
