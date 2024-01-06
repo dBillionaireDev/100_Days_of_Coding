614cbe63a80941366ff27562fd9c42e259e4870a
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-06 15:26:43 +0100

    The Level 2

--- 30_Days_of_Javascript/Day-4/scripts/main.js
+++ 30_Days_of_Javascript/Day-4/scripts/main.js
@@ -52,3 +52,32 @@ number % 2 === 0
 ? console.log(`${number} is an even number`)
 : console.log(`${number} is an odd number`)
 
+//Level 2
+/*Task 1: Write a code which can give grades to students according to theirs scores:
+80-100, A
+70-79, B
+60-69, C
+50-59, D
+0-49, F
+*/
+
+let num = prompt('Enter your score', 'Student grade');
+switch (true) {
+  case num >= 80:
+    console.log('You have an A');
+    break;
+  case num <= 79 >= 70:
+    console.log('You have a B');
+    break;
+case num <= 69 >= 60:
+    console.log('You have a C');
+    break;
+case num <= 59 >= 50:
+    console.log('You have a D');
+    break;
+  case num <= 49:
+    console.log('You have an F');
+    break;
+  default:
+    console.log('You have no grade');
+}
