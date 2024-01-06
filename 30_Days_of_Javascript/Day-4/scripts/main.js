747ad954222b6cc2cce7e65880cf5cf92f102177
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-06 16:00:01 +0100

    Day $ done

--- 30_Days_of_Javascript/Day-4/scripts/main.js
+++ 30_Days_of_Javascript/Day-4/scripts/main.js
@@ -163,4 +163,49 @@ switch (day) {
     console.log(`${day} is not a day of the week.`)
 }
 
+//Level 3
+//Task 1: Write a program which tells the number of days in a month.
 
+userInput = prompt('What month is it?')
+month = userInput.toLowerCase()
+
+switch (month) {
+ case 'september':
+    console.log('September has 30 days')
+    break
+ case 'october': 
+    console.log('October has 31 days')
+    break
+ case 'november':
+    console.log('November has 30 days')
+    break
+ case 'december':
+    console.log('December has 31 days')
+    break
+ case 'january':
+    console.log('January has 31 days.')
+    break
+ case 'february':
+    console.log('February has 28 days')
+    break
+ case 'march':
+    console.log('March has 31 days')
+    break
+ case 'april':
+    console.log('April has 30 days')
+    break
+  case 'may':
+    console.log('May has 31 days')
+    break
+  case 'june':
+    console.log('June has 30 days')
+    break
+  case 'july':
+    console.log('July has 31 days')
+    break
+  case 'august':
+    console.log('August has 31 days')
+    break
+  default:
+    console.log(`${month} is not a month.`)
+}
