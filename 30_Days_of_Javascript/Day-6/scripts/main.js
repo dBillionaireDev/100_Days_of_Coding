53f9da44f1ee87caebc8778c98b2d6d675e0b8e3
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-12 22:09:46 +0100

    Changes made

--- 30_Days_of_Javascript/Day-6/scripts/main.js
+++ 30_Days_of_Javascript/Day-6/scripts/main.js
@@ -1,2 +1,31 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+//For loops
+
+for(let i = 0; i <=10; i++){
+	console.log(i)
+}
+
+for(let i = 10; i >=0; i--){
+	console.log(i)
+}
+
+//While loops
+
+let i = 0;
+while(i <= 10){
+	console.log(i);
+	i++;
+}
+
+let i = 10;
+while(i >= 0){
+	console.log(i);
+	i--;
+}
+
+for (let i = 1; i <= 7; i++) {
+  let hashString = '#'.repeat(i);
+  console.log(hashString);
+}
