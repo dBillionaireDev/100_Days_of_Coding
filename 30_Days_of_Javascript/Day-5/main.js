5389ccc3a251df3c70f4444a9d066d878f643362
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-12 19:49:26 +0100

    NO. 3

--- 30_Days_of_Javascript/Day-5/main.js
+++ 30_Days_of_Javascript/Day-5/main.js
@@ -2,3 +2,11 @@
 
 let emptyArray = []
 let arr = [3, 5, 6, 9, 21, 15, 15, 7] 
+const arrLength = arr.length
+
+let firstNum = arr[0];
+let middleNum = arr[3];
+let lastNum = arr.length -1;
+
+console.log(firstNum, middleNum, lastNum);
+
