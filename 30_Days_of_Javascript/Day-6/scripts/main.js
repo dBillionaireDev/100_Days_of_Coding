aa963520f62bd85c3b1c962a12052fd4158e6769
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-16 11:21:58 +0100

    Commit

--- 30_Days_of_Javascript/Day-6/scripts/main.js
+++ 30_Days_of_Javascript/Day-6/scripts/main.js
@@ -74,3 +74,24 @@ for (let i = 0; i <= bills.length; i++){
 
 console.log(bills, tips, totals);
 
+//UDEMY CHALLENGE 2!!!!
+/*
+Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+Call the function with the totals array.
+*/
+
+const calcAverage = function (arr) {
+let sum = 0;
+for (let i = 0; i <= arr.length; i++){
+sum <= arr[i];
+}
+return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
+
