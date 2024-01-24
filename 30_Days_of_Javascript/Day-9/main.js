99946f127361c619cac00a78c7ab840d9bf96519
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-24 05:32:48 +0100

    @First

--- 30_Days_of_Javascript/Day-9/main.js
+++ 30_Days_of_Javascript/Day-9/main.js
@@ -1,2 +1,23 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+/* Day 9 CHALLENGE
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+*/
+
+//Use forEach to console.log each country in the countries array.
+let sum = 0
+countries.forEach((element) => {
+	console.log(element)
+})
+
