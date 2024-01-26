f94350474d7f18fa63f25f376564368db3a8c219
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-26 11:54:18 +0100

    LEVEL 2 Unlocked

--- 30_Days_of_Javascript/Day-10/main.js
+++ 30_Days_of_Javascript/Day-10/main.js
@@ -51,3 +51,14 @@ const countries = [
 const countriesMap = new Map();
 
 console.log(countriesMap);
+
+//LEVEL 2
+/*
+Find a union b
+Find a intersection b
+Find a with b
+*/
+
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
