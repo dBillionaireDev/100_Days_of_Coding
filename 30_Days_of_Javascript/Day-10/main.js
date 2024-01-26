675c7e5ab6d2405dfb593f37aa20410c90f0df7a
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-26 10:44:55 +0100

    Tasks

--- 30_Days_of_Javascript/Day-10/main.js
+++ 30_Days_of_Javascript/Day-10/main.js
@@ -1,3 +1,13 @@
 console.log("JavaScript is Cool");
 
 //DAY 10 CHALLENGES
+
+/*create an empty set
+Create a set containing 0 to 10 using loop
+Remove an element from a set
+Clear a set
+Create a set of 5 string elements from array
+Create a map of countries and number of characters of a country
+*/
+
+
