b9f9ee43da4f5fc7213beb89ab4601f81705b458
Nezer-Ekunke <nezerekunke@gmail.com>
2024-01-26 23:02:30 +0100

    Task 1 completed

--- 30_Days_of_Javascript/Day-11/main.js
+++ 30_Days_of_Javascript/Day-11/main.js
@@ -1,2 +1,68 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+//DAY 11 CHALLENGES
+
+const constants = [2.72, 3.14, 9.81, 37, 100]
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60
+}
+const users = [
+{
+  name:'Brook',
+  scores:75,
+  skills:['HTM', 'CSS', 'JS'],
+  age:16
+},
+{
+  name:'Alex',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'David',
+  scores:75,
+  skills:['HTM', 'CSS'],
+  age:22
+},
+{
+  name:'John',
+  scores:85,
+  skills:['HTML'],
+  age:25
+},
+{
+  name:'Sara',
+  scores:95,
+  skills:['HTM', 'CSS', 'JS'],
+  age: 26
+},
+{
+  name:'Martha',
+  scores:80,
+  skills:['HTM', 'CSS', 'JS'],
+  age:18
+},
+{
+  name:'Thomas',
+  scores:90,
+  skills:['HTM', 'CSS', 'JS'],
+  age:20
+}
+]
+
+/*
+Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+Destructure and assign the elements of countries array to fin, est, sw, den, nor
+Destructure the rectangle object by its properties or keys.
+*/
+
+//Task 1: Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+
+const [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp)
